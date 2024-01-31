a18d54703f6952a9f5382dc9b73bd130918147d9
jhonegratieri <jhonatansimoni@gmail.com>
2024-01-31 17:10:04 -0300

    api views and serializers.Serializer

--- recipes/urls.py
+++ recipes/urls.py
@@ -40,4 +40,19 @@ urlpatterns = [
         views.RecipeDetailAPI.as_view(),
         name="recipes_api_v1_detail",
     ),
+    path(
+        "recipes/api/v2/",
+        views.recipe_api_list,
+        name="recipes_api_v2",
+    ),
+    path(
+        "recipes/api/v2/<int:pk>/",
+        views.recipe_api_detail,
+        name="recipes_api_v2_detail",
+    ),
+    path(
+        "recipes/api/v2/tag/<int:pk>/",
+        views.tag_api_detail,
+        name="recipes_api_v2_tag",
+    ),
 ]
