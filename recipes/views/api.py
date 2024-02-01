482157c7fa10a16357299a8570256c3cb137665e
jhonegratieri <jhonatansimoni@gmail.com>
2024-02-01 10:29:47 -0300

    POST method in api view

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,5 +1,6 @@
 # from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -10,13 +11,22 @@ from tag.models import Tag
 
 @api_view()
 def recipe_api_list(request):
-    recipes = Recipe.objects.get_published()[:10]
-    serializer = RecipeSerializer(
-        instance=recipes,
-        many=True,
-        context={"request": request},
-    )
-    return Response(serializer.data)
+    if request.method == "GET":
+        recipes = Recipe.objects.get_published()[:10]
+        serializer = RecipeSerializer(
+            instance=recipes,
+            many=True,
+            context={"request": request},
+        )
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = RecipeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            serializer.validated_data,
+            status=status.HTTP_201_CREATED,
+        )
 
 
 @api_view()
