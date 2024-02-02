f808f5a0ba7a80a57f56f8803b547bf093219da3
jhonegratieri <jhonatansimoni@gmail.com>
2024-02-02 17:15:13 -0300

    using viewsets

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,10 +1,9 @@
 # from rest_framework import status
 from django.shortcuts import get_object_or_404
-from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer, TagSerializer
@@ -15,18 +14,12 @@ class RecipeAPIv2Pagination(PageNumberPagination):
     page_size = 2
 
 
-class RecipeAPIv2List(ListCreateAPIView):
+class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
 
-
-class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
-    queryset = Recipe.objects.get_published()
-    serializer_class = RecipeSerializer
-    pagination_class = RecipeAPIv2Pagination
-
-    def patch(self, request, *args, **kwargs):
+    def partial_update(self, request, *args, **kwargs):
         pk = kwargs.get("pk")
         recipe = self.get_queryset().filter(pk=pk).first()
         serializer = RecipeSerializer(
@@ -41,11 +34,6 @@ class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
         return Response(
             serializer.data,
         )
-
-    def delete(self, request, pk):
-        recipe = self.get_recipe(pk)
-        recipe.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
